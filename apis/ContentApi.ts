f275390e1988cb7157044d0605e8f3dac36d5b41
praveenkumarct <praveen.kumar@commercetools.com>
2023-08-03 01:09:22 +0200

    Fix format and adjust code to fetch Bloomreach content

--- apis/ContentApi.ts
+++ apis/ContentApi.ts
@@ -1,34 +1,30 @@
 import { Context } from '@frontastic/extension-types';
-import axios from "axios";
-import {initialize} from "@bloomreach/spa-sdk";
-import { Content } from '@Types/content/Content';
-import {ContentMapper} from "@Content-bloomreach/mappers/ContentMapper";
+import axios from 'axios';
+import { initialize } from '@bloomreach/spa-sdk';
+import { ContentMapper } from '@Content-bloomreach/mappers/ContentMapper';
 
 export default class ContentApi {
   private page: any;
   private locale: string;
+  private deliveryApiUrl: string;
 
   constructor(frontasticContext: Context, locale?: string) {
     this.locale = (locale ?? frontasticContext.project.defaultLocale).replace('_', '-');
+    this.deliveryApiUrl = frontasticContext.project.configuration?.bloomreach.deliveryApiUrl;
   }
 
-  async getContent(): Promise<Content> {
-
-    const deliveryApiUrl = 'https://sandbox-commercetools.bloomreach.io/delivery/site/v1/';
-    const path = ``;
-
+  async getContent() {
+    const path = '';
     // Initialize Bloomreach
     this.page = await initialize({
       // The path to request from the Page Model API, should include query
       // parameters if those are present in the url
       path,
       // The location of the Page Model API of the brX channel
-      endpoint: `${deliveryApiUrl}channels/commercetools/pages`,
+      endpoint: `${this.deliveryApiUrl}channels/commercetools/pages`,
       // The httpClient used to make requests
       httpClient: axios,
     });
-
-    return ContentMapper.bloomreachDocumentToContent(this.page.getDocument().getData());
+    return ContentMapper.bloomreachDocumentToContent(this.page);
   }
-
 }
--- mappers/ContentMapper.ts
+++ mappers/ContentMapper.ts
@@ -2,15 +2,24 @@ import { Content } from '@Types/content/Content';
 
 export class ContentMapper {
   static bloomreachDocumentToContent(response: any): Content {
+    response = JSON.parse(JSON.stringify(response));
 
-    const { title, introduction, image } = response;
+    const pageRef = response.root.$ref.split('/').pop();
+    const pageData = response.page[pageRef];
+
+    const documentRef = response.document?.$ref.split('/').pop();
+    const documentData = response.page[documentRef];
+    const { id, title, introduction, image } = documentData.data;
+
+    const imageRef = image.$ref.split('/').pop();
+    const imageLinks = response.page[imageRef].data.original?.links;
 
     return {
-      contentId: "",
-      contentTypeId: "",
+      contentId: pageData.id,
+      contentTypeId: id,
       title: title,
-      banner: image.$ref,
-      summary: introduction
+      banner: imageLinks?.site?.href,
+      summary: introduction,
     };
   }
 }
